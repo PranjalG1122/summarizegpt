4483cd1de0f49083e2e6a94ee84f18df44c96367
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-03-24 13:54:58 +0530

    feat: frontend for summarizing chatgpt api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,68 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
 
+async function res(prompt: object) {
+  await fetch("/api/gpt", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+    }),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.content);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 export default function Home() {
+  const [userText, setUserText] = useState("");
+  const summaryMessages = [
+    {
+      username: "John",
+      messages: [
+        "There is an important meeting on the 7th at 10pm IST.",
+        "What's the agenda? for today's meeting?",
+      ],
+    },
+    {
+      username: "Jane",
+      messages: [
+        "There is an important meeting on the 9th at 2pm IST.",
+        "My cat just had kittens",
+      ],
+    },
+    {
+      username: "Joey",
+      messages: [
+        "There is an important seminar on the 10th at 5pm IST.",
+        "I'm going to the movies tonight",
+      ],
+    },
+  ];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <input
+        type="text"
+        onChange={(e) => {
+          setUserText(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          res(summaryMessages);
+        }}
+      >
+        Click for gpt fun
+      </button>
     </main>
-  )
+  );
 }

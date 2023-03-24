08dc9f15e2d2a44da4cd289d1b05f80abfee341e
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-03-24 20:09:35 +0530

    feat: client can now add custom user/messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,31 @@
 
 import { useState } from "react";
 
+interface messageProps {
+  id: number;
+  username: string;
+  message: string;
+}
+
 export default function Home() {
   const [summarizedMessage, setSummarizedMessage] = useState<string>("");
   const [loadingMessages, setLoadingMessages] = useState<boolean>(false);
+  const [allMessages, setAllMessages] = useState<messageProps[]>([
+    {
+      id: 1,
+      username: "Jane",
+      message:
+        "There is an important meeting on the 9th at 2pm IST. My cat just had kittens.",
+    },
+    {
+      id: 2,
+      username: "Joey",
+      message:
+        "There is an important seminar on the 10th at 5pm IST. I'm going to the movies tonight.",
+    },
+  ]);
+  const [username, setUsername] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
   async function res(prompt: object) {
     await fetch("/api/gpt", {
@@ -27,29 +49,6 @@ export default function Home() {
         console.error(err);
       });
   }
-  const summaryMessages = [
-    {
-      username: "John",
-      messages: [
-        "There is an important meeting on the 7th at 10pm IST.",
-        "What's the agenda? for today's meeting?",
-      ],
-    },
-    {
-      username: "Jane",
-      messages: [
-        "There is an important meeting on the 9th at 2pm IST.",
-        "My cat just had kittens",
-      ],
-    },
-    {
-      username: "Joey",
-      messages: [
-        "There is an important seminar on the 10th at 5pm IST.",
-        "I'm going to the movies tonight",
-      ],
-    },
-  ];
 
   return (
     <main className="flex flex-col gap-6 items-center justify-center min-h-screen w-full max-w-[72rem] mx-auto my-10">
@@ -61,45 +60,71 @@ export default function Home() {
         paragraph containing pertinent information, such as important meetings,
         dates, timings and more.
       </p>
-      {summaryMessages.map((message) => {
+      {allMessages.map((message, index) => {
         return (
           <div className="flex flex-col items-start rounded p-4 bg-slate-700 text-white w-full">
             <h1 className="font-semibold text-xl">{message.username}</h1>
-            <ul>
-              {message.messages.map((message: string, index: number) => {
-                return (
-                  <li key={index} className="text-lg">
-                    - {message}
-                  </li>
-                );
-              })}
-            </ul>
+            <p key={index} className="text-lg">
+              - {message.message}
+            </p>
           </div>
         );
       })}
-      <div className="flex flex-row w-full gap-4">
+      <div className="flex flex-col gap-2 items-start bg-slate-700 w-full p-4 rounded">
         <input
           type="text"
-          className="bg-slate-700 px-1 py-0.5 text-white placeholder:text-slate-400 rounded w-full"
-          placeholder="Enter your message here..."
+          className="bg-slate-600 px-1 py-0.5 text-white placeholder:text-slate-400 rounded w-96"
+          placeholder="Enter a test username"
+          maxLength={20}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          value={username}
+        />
+        <input
+          type="text"
+          className="bg-slate-600 px-1 py-0.5 text-white placeholder:text-slate-400 rounded w-full"
+          placeholder="Enter a test message"
+          maxLength={60}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          value={message}
         />
         <button
+          className="bg-slate-600 px-2 py-1 font-semibold hover:bg-slate-500 transition-colors rounded text-white mx-auto"
           onClick={() => {
-            setLoadingMessages(true);
-            res(summaryMessages);
+            setAllMessages([
+              ...allMessages,
+              {
+                id: allMessages.length + 1,
+                username: username,
+                message: message,
+              },
+            ]);
+            setUsername("");
+            setMessage("");
           }}
-          disabled={loadingMessages}
-          className="bg-slate-700 px-2 py-1 font-semibold hover:bg-slate-600 transition-colors rounded text-white"
         >
-          {loadingMessages ? "Loading..." : "Summarize"}
+          Add Message to Summarize
         </button>
       </div>
-      <div className="flex flex-col border-4 border-slate-600 items-center text-white rounded bg-slate-700 p-2 gap-2">
-        <h1 className="text-xl font-semibold">Summarized Message</h1>
-        <p className="text-lg">
-          {summarizedMessage}
-        </p>
-      </div>
+      <button
+        onClick={() => {
+          setLoadingMessages(true);
+          res(allMessages);
+        }}
+        disabled={loadingMessages}
+        className="bg-slate-700 px-2 py-1 font-semibold hover:bg-slate-600 transition-colors rounded text-white mx-auto"
+      >
+        {loadingMessages ? "Loading..." : "Summarize"}
+      </button>
+      {(summarizedMessage && (
+        <div className="flex flex-col border-4 border-slate-600 items-center text-white rounded bg-slate-700 p-2 gap-2">
+          <h1 className="text-xl font-semibold">Summarized Message</h1>
+          <p className="text-lg">{summarizedMessage}</p>
+        </div>
+      )) || <></>}
     </main>
   );
 }

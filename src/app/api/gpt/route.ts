e6f05e2d00e03e48f1252632030ac47db47d964f
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-03-25 10:35:00 +0530

    fix: added try catch

--- src/app/api/gpt/route.ts
+++ src/app/api/gpt/route.ts
@@ -16,21 +16,26 @@ export async function POST(request: Request, response: Response) {
     }
   );
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are summarizeGPT. You summarize messages into a concise and informative paragraph containing only important information from other users and ignore anything irrelevant .",
-      },
-      ...body,
-      {
-        role: "system",
-        content: "Summarize the above messages into a single paragraph.",
-      },
-    ],
-  });
-  console.log(completion.data.choices[0].message);
-  return new Response(JSON.stringify(completion.data.choices[0].message));
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      max_tokens: 1000,
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are summarizeGPT. You summarize messages into a concise and informative paragraph containing only important information from other users and ignore anything irrelevant .",
+        },
+        ...body,
+        {
+          role: "system",
+          content: "Summarize the above messages into a single paragraph.",
+        },
+      ],
+    });
+    console.log(completion.data.choices[0].message);
+    return new Response(JSON.stringify(completion.data.choices[0].message));
+  } catch (e) {
+    return new Response(JSON.stringify("error"));
+  }
 }
